007512505a2e81f3f3b02c1e361b5bbd340226c2
Dimitar Stoyanov <47748614+ZoomMan12312@users.noreply.github.com>
2019-03-24 14:04:41 +0200

    Add files via upload

--- basic_prj/main.py
+++ basic_prj/main.py
@@ -13,19 +13,56 @@ scoreint = nb.scoreInterval(x_con,1,y,10)
 print(scoreint)
 plot_false = []
 plot_total = []
+plot_err = []
+plot_cml = []
+plot_cml_err = []
 names = []
+total = 0
+total_err = 0
+
+for i in scoreint:
+    total += i[3]
+    total_err += i[2]
 
 import matplotlib.pyplot as plt
+cnt = 0
 for i in scoreint:
+    err = 0
+    if i[2] and i[3] != 0:
+        err = i[2]/i[3]
+    plot_err.append(err)
+
+    if cnt != 0:
+        cm_to_add = i[3]/total + plot_cml[cnt-1]
+        cm_to_add_err = i[2]/total_err + plot_cml_err[cnt-1]
+    else:
+        cm_to_add = i[3]/total
+        cm_to_add_err = i[2]/total_err
+
+    plot_cml_err.append(cm_to_add_err)
+    plot_cml.append(cm_to_add)
+    cnt += 1
+
     plot_false.append(i[2])
     plot_total.append(i[3])
     names.append(str(int(i[0]*100)) + '-' + str(int(i[1]*100)))
 
 #x = range(len(plot_false))
-
+plt.subplot(121)
+plt.title('Err/Observations')
 plt.bar(names, plot_false)
-plt.plot(plot_total, 'r--')
-plt.plot(plot_total, 'ro')
+plt.plot(plot_total, 'g--')
+plt.plot(plot_total, 'go')
+plt.grid(True)
+plt.subplot(122)
+plt.title('(r)Err%/(g)Comulative Observations/(y)Comulative Errors')
+plt.plot(names, plot_err, 'r--')
+plt.plot(plot_err, 'ro')
+plt.plot(plot_cml, 'g--')
+plt.plot(plot_cml, 'go')
+plt.plot(plot_cml_err, 'y--')
+plt.plot(plot_cml_err, 'yo')
+plt.grid(True)
 plt.show()
 '''
 pred = nb.predict([80, 173], 0)
